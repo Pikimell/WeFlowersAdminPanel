1e8a8356ca70a3a62cfbe13a61481e4379a51946
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-05-11 23:25:43 +0300

    update

--- src/js/pages/products.js
+++ src/js/pages/products.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
+import axios from 'axios';
+import { uid } from 'uid';
+import { toBase64 } from '../modules/helpers.js';
 
 const refs = {
   formElem: document.querySelector('.js-form'),
   previewElem: document.querySelector('.js-preview'),
   smallPreviewElem: document.querySelector('.js-small-preview'),
+  testBtnElem: document.querySelector('.js-save-img'),
 };
 
 refs.formElem.addEventListener('input', _.throttle(onInputChange, 300));
@@ -62,4 +66,21 @@ refs.formElem.addEventListener('change', function (e) {
     console.log(err);
     console.log('Ple Enter Title');
   }
+});
+
+refs.testBtnElem.addEventListener('click', async () => {
+  const BASE_URL = `http://localhost:3000/dev`;
+
+  const url = `${BASE_URL}/files`;
+
+  const files = refs.formElem.elements.image.files;
+  const file = files[0];
+  const params = new URLSearchParams({
+    fileName: uid() + file.name.slice(-4),
+    fileType: file.type,
+  });
+  if (files.length == 0) return;
+
+  const base64 = await toBase64(file);
+  axios.post(`${url}?${params}`, base64);
 });
